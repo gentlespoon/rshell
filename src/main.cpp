b0b779d8f56bde50843418f41f9ee5ffa7d89313
Angda Song <asong011@ucr.edu>
2015-10-24 13:13:59 -0700

    Finished Quotation and Comments

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,13 @@
 #include <vector>
 // #include <windows.h>
 
+bool DEV = true;
+// global debug output control added.
+// default DEV = true;
+// can be switched by "$ debug [on|off]"
+
 using namespace std;
 
-  
   // Initialized or testing purpose: running code in Windows to emulate getuserinfo() gethostname()
   string user = "asong011";
   char host[128] = "windows.local";
@@ -22,14 +26,13 @@ void init() {
 }
 
 int newCmd() {
-  // vector<pair cmd> cmdList;
   cout << "\n[" << user << "@" << host << "] R$ ";
   // to distinguish with system shell I used "R$" instead of "$"
   
+  // GET USER INPUT
   string newLine;
   getline(cin, newLine);
-  cout << 
-"\n========== Start Parsing ==========\n\n[Raw Input] " << newLine << endl;
+  if (DEV) cout << "\n========== Start Parsing ==========\n\n[Raw Command] " << newLine << endl;
 
   // debloat comments ** with bug: comments in "#".
 
@@ -62,32 +65,64 @@ int newCmd() {
   cout << "[Trimmed #] " << newLine << endl;
 */
 
+
+  // Finished Quotation and Comments
+  // ====== bug exist ======
+  // For addresses that use \ instead of / (e.g. Windows), the program may mess up with escape characters
   for (unsigned index = 0; index < newLine.length(); index++) {
+    
+    // Dealing with "" quotes
     if (newLine.at(index) == '\"') {
-      cout << "\" found at position " << index;
+      if (DEV) cout << "\" found at position " << index;
       if ((index == 0) || (newLine.at(index-1) != '\\')) {
-        cout << ", this is a real quotation mark." << endl;
+        if (DEV) cout << " This is a real quotation mark." << endl;
         isInQuote = !isInQuote;
-        cout << "[isInQuote] " << isInQuote << endl;
+        if (DEV) cout << "[isInQuote] " << isInQuote << endl;
       }
       else {
-        cout << ", this is a slashed quotation mark." << endl;
-        cout << "[isInQuote] " << isInQuote << endl;
+        if (isInQuote) {
+          if (DEV) cout << " This is a slashed quotation mark in a quote." << endl;
+        }
+        else {
+          cout << "\nWARNING: There might be a slashed quotation mark outside a quote!" << endl;
+        }
+        if (DEV) cout << "[isInQuote] " << isInQuote << endl;
+      }
+    }
+
+    // Dealing with # comments
+    if (newLine.at(index) == '#') {
+      if (DEV) cout << "# found at position " << index;
+      if (isInQuote) {
+        if (DEV) cout << ", however this # is in a Quote. Ignored.\n";
+      }
+      else {
+        if (DEV) cout << ", wow we found a comment!";
+        newLine = newLine.substr(0, index);
+        break;
       }
     }
   }
 
 
+  // Finished parsing line
 
+  if (isInQuote) { // if when the line ends we are still in a quote
+    cout << "\nWARNING: The line ends in a quote. \" expected.\n";
+  }
 
-
-
-
-
-  cout << "\n========== End Parsing =========\n\n[Final Command] " << endl << newLine << endl;
+  if (DEV) cout << "\n[Final Command] " << newLine << "\n\n=========== End Parsing ===========\n";
 
   if (newLine == "exit") {
     return -1; // use -1 as a exit signal
+  }
+  else if (newLine == "debug on") {
+    DEV = true;
+    cout << "Debug output is now turned on." << endl;
+  }
+  else if (newLine == "debug off") {
+    DEV = false;
+    cout << "Debug output is now turned off." << endl;
   }
   else {
     return 0;
